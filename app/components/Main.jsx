97280c4ad1c9bc79b4ffbef155c331a616ae2920
Rent Sidon <rent.sidon@gmail.com>
2017-04-07 11:08:04 -0700

    styled nav bar

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,11 +1,12 @@
 var React = require('react');
+var Nav = require('Nav');
 
 var Main = (props) => {
   return (
     <div>
       <div>
         <div>
-          <p>Main.jsx Rendered</p>
+          <Nav/>
           {props.children}
         </div>
       </div>
