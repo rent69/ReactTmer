d887c793e008964118ea0157a717dbf11c0416f2
Rent Sidon <rent.sidon@gmail.com>
2017-04-10 13:35:48 -0700

    Created countdown form component and test

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,14 +1,27 @@
 var React = require('react');
 var ReactRouter = require('react-router');
 var Clock = require('Clock');
+var CountdownForm = require('CountdownForm');
 
-var Countdown = (props) => {
-  return (
+var Countdown = React.createClass ({
+  getInitialState: function () {
+    return {count: 0};
+  },
+  handleSetCountdown: function (seconds) {
+    this.setState ({
+      count: seconds
+    });
+
+  },
+  render: function () {
+    var {count} = this.state;
+   return (
     <div>
-    <Clock totalSeconds={129}/>
+    <Clock totalSeconds={count}/>
+    <CountdownForm onSetCountdown={this.handleSetCountdown}/>
     </div>
-
   );
 }
+});
 
 module.exports = Countdown;
